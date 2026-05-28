5c5b567ad00f8e1ac55dcb1e574a841e3544ec13
jryingling <yinglingjry@gmail.com>
2026-05-28 18:08:36 -0400

    presets + config for router specific info

--- model/profile.go
+++ model/profile.go
@@ -11,8 +11,10 @@ const (
 
 /*
 preset choice gives us ruleset for that preset
+router pointer to routerconfig object, for required router wan, lan, subnet info
+optional by passing nil for other presets
 */
-func DefaultRuleset(preset ProfileType) Ruleset {
+func DefaultRuleset(preset ProfileType, router *RouterConfig) Ruleset {
 	switch preset {
 		case Desktop:
 			return Ruleset{
@@ -20,7 +22,6 @@ func DefaultRuleset(preset ProfileType) Ruleset {
 				Input: Chain{
 					ChainName: INPUT,
 					Policy: DROP,
-					// conntrack, interfaces, protocols (order matters)
 					Rules: []Rule{
 						{
 							Comment: "Allow established/related connections",
@@ -52,9 +53,123 @@ func DefaultRuleset(preset ProfileType) Ruleset {
 				},
 			}
 		case Server:
-			return Ruleset{}
+			return Ruleset{
+				RulesetName: "Server",
+				Input: Chain{
+					ChainName: INPUT,
+					Policy: DROP,
+					Rules: []Rule{
+						{
+							Comment: "Allow established/related connections",
+							ConntrackState: "ESTABLISHED,RELATED",
+							Target: ACCEPT,
+							
+						},
+						{
+							Comment: "Allow loopback traffic on Host",
+							InIface: "lo",
+							Target: ACCEPT,
+						},
+						{
+							Comment: "Allow ICMP",
+							Protocol: ICMP,
+							Target: ACCEPT,
+						},
+						{
+							Comment: "Allow SSH",
+							Protocol: TCP,
+							DstPort: "22",
+							Target: ACCEPT,
+						},
+						{
+							Comment: "Allow HTTP",
+							Protocol: TCP,
+							DstPort: "80",
+							Target: ACCEPT,
+						},
+						{
+							Comment: "Allow HTTPS",
+							Protocol: TCP,
+							DstPort: "443",
+							Target: ACCEPT,
+						},
+					},
+				},
+				Output: Chain {
+					ChainName: OUTPUT,
+					Policy: ACCEPT,
+					Rules: []Rule{},
+				},
+				Forward: Chain {
+					ChainName: FORWARD,
+					Policy: DROP,
+					Rules: []Rule{},
+				},
+			}
 		case Router:
-			return Ruleset{}
+			var wanIf, lanIf string
+			if router == nil {
+				wanIf = "eth0"
+				lanIf = "eth1"
+			} else {
+				wanIf = router.WanIF
+				lanIf = router.LanIF
+			}
+			return Ruleset{
+				RulesetName: "Router",
+				Input: Chain{
+					ChainName: INPUT,
+					Policy: DROP,
+					Rules: []Rule{
+						{
+							Comment: "Allow established/related connections",
+							ConntrackState: "ESTABLISHED,RELATED",
+							Target: ACCEPT,
+							
+						},
+						{
+							Comment: "Allow loopback traffic on Host",
+							InIface: "lo",
+							Target: ACCEPT,
+						},
+						{
+							Comment: "Allow ICMP",
+							Protocol: ICMP,
+							Target: ACCEPT,
+						},
+						{
+							Comment: "Allow SSH",
+							Protocol: TCP,
+							DstPort: "22",
+							InIface: wanIf,
+							Target: ACCEPT,
+						},
+					},
+				},
+				Output: Chain {
+					ChainName: OUTPUT,
+					Policy: ACCEPT,
+					Rules: []Rule{},
+				},
+				Forward: Chain {
+					ChainName: FORWARD,
+					Policy: DROP,
+					Rules: []Rule{
+						{
+							Comment: "Allow established/related connections",
+							ConntrackState: "ESTABLISHED,RELATED",
+							Target: ACCEPT,
+							
+						},
+						{
+							Comment: "LAN > WAN",
+							InIface: lanIf,
+							OutIface: wanIf,
+							Target: ACCEPT,
+						},
+					},
+				},
+			}
 		case Custom:
 			/*
 				empty ruleset with 
